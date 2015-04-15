1f26ac2724ee7469301589cee5763cb2700b0b34
Jose Saul Giffuni Becerra <giffunis@gmail.com>
2015-04-15 18:34:20 +0100

    Pruebas Unitarias realizadas con exito

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,15 +1,24 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
+from django import forms
+
+# Leyenda de las clases
+# Campo de caracter obligatorio: *
+
+# Fin de la leyenda
+
+
 
 
 # Create your models here.
 class Usuario(models.Model):
-    nombre = models.CharField(max_length=30)
-    apellidos = models.CharField(max_length=30)
-    pseudonimo = models.CharField(max_length=15)
-    correo = models.EmailField(max_length=30)
-    password = models.CharField(max_length=30)
+    nombre = models.CharField(max_length = 2) # *
+    apellidos = models.CharField(max_length = 30) # *
+    pseudonimo = models.CharField(primary_key = True, max_length = 30) # *
+    correo = models.EmailField(max_length = 30) # *
+    password = models.CharField(max_length = 30) # *
+    #date = models.DateField() # *
 
     def __str__(self):
         cadena = 'Nombre: ' + self.nombre + ', Apellidos: ' + self.apellidos + ', Pseudonimo: ' + self.pseudonimo + ', Correo: ' + self.correo + ', Contraseña: ' + self.password
--- usuarios/tests.py
+++ usuarios/tests.py
@@ -1,3 +1,41 @@
 from django.test import TestCase
+from usuarios.models import Usuario
 
 # Create your tests here.
+
+class UsuarioTestCase(TestCase):
+
+    def setUp(self):
+        Usuario.objects.create(nombre = "Alejandro", apellidos = "Ravelo", pseudonimo = "J", correo = "alu@ull.edu.es", password = "batman")
+        Usuario.objects.create(nombre = "Joaquin", apellidos = "Escobar", pseudonimo = "Jo", correo = "alu2@ull.edu.es", password = "robin")
+
+    def test_tiene_nombre(self):
+        u1 = Usuario.objects.get(apellidos = 'Ravelo' )
+        self.assertEqual(u1.nombre, 'Alejandro')
+
+    def test_tiene_Apellido(self):
+        u2 = Usuario.objects.get(nombre = 'Joaquin')
+        self.assertEqual(u2.apellidos, 'Escobar')
+
+    def test_Nombre_no_vacio(self):
+        u1 = Usuario.objects.get(apellidos = 'Escobar')
+        self.assertNotEqual(u1.nombre,"")
+
+    def test_Apellido_no_vacio(self):
+        u1 = Usuario.objects.get(nombre = 'Joaquin')
+        self.assertNotEqual(u1.apellidos,"")
+
+    def test_Pseudonimo_no_vacio(self):
+        u1 = Usuario.objects.get(nombre = 'Joaquin')
+        self.assertNotEqual(u1.pseudonimo,"")
+
+    def test_Correo_no_vacio(self):
+        u1 = Usuario.objects.get(nombre = 'Joaquin')
+        self.assertNotEqual(u1.correo,"")
+
+    def test_Password_no_vacio(self):
+        u1 = Usuario.objects.get(nombre = 'Joaquin')
+        self.assertNotEqual(u1.password,"")
+
+    #def test_disponibilidad_pseudonimo(self)
+    #    assertNotEqual()
