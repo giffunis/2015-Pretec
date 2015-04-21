585afc1f679647f0a33e065f02daf5778fb8f08e
J.Joaquin <quinoescobar@gmail.com>
2015-04-21 13:41:05 +0100

    agregado en el views.py el médoto para obtener el nombre de su forms

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+
+from .forms import FormNombre
 
 # Create your views here.
+def get_nombre(request)
+    if request.method == 'POST':
+        #crea una instancia de formulario y la llena con los datos del request
+        form=FormNombre(request.POST)
+        #verifica si es valido
+        if form.is_valid():
+            return HttpResponseRedirect('/thanks/')
+    else:
+        form = FormNombre()
+
+    return render(request, 'registro.html', {'form' : form})
 
 def formulario_registro(request):
     return render(request, 'formulario_registro.html')
