8b3739a40741c954bdd5c3242dd8249079a47662
J.Joaquin <quinoescobar@gmail.com>
2015-04-22 11:06:43 +0100

    modificado el views

--- usuarios/views.py
+++ usuarios/views.py
@@ -7,14 +7,16 @@ from .forms import RegistroForm
 def get_nombre(request):
     if request.method == 'POST':
         #crea una instancia de formulario y la llena con los datos del request
-        form=FormNombre(request.POST)
+        form=RegistroForm(request.POST)
         #verifica si es valido
         if form.is_valid():
             return HttpResponseRedirect('/thanks/')
     else:
-        form = FormNombre()
+        form = RegistroForm()
 
     return render(request, 'formulario_registro.html', {'form' : form})
+
+
 
 def formulario_registro(request):
     return render(request, 'formulario_registro.html')
