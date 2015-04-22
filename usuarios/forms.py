735a7d12965758110e279f980e0ba4f5a9d016cb
J.Joaquin <quinoescobar@gmail.com>
2015-04-22 10:24:29 +0100

    Funcionando el views con el forms

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -1,18 +1,12 @@
 from django import forms
+from django.db import models
 
 
-class RegistroForm(forms.form)
+class RegistroForm(forms.Form):
   nombre = forms.CharField(label='Nombre', max_length=30, min_length=2)
   apellidos = forms.CharField(label = 'Aplellidos', max_length = 50)
   pseudonimo = forms.CharField(label= 'Pseudonimo', max_length='20', min_length='5')
   correo = forms.EmailField(label='correo')
   password1 = forms.CharField(label='Contrasena', widget=forms.PasswordInput)
   password2 = forms.CharField(label='Repita la contrasena', widget=forms.PasswordInput)
-  date = forms.dateField()
-  
-  
-  
-
-  
-  
-  
+  date = forms.DateField() #---> Esta dando fallos, dice que no hay atributo dateField() en models
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 
-from .forms import FormNombre
+from .forms import RegistroForm
 
 # Create your views here.
-def get_nombre(request)
+def get_nombre(request):
     if request.method == 'POST':
         #crea una instancia de formulario y la llena con los datos del request
         form=FormNombre(request.POST)
@@ -14,7 +14,7 @@ def get_nombre(request)
     else:
         form = FormNombre()
 
-    return render(request, 'registro.html', {'form' : form})
+    return render(request, 'formulario_registro.html', {'form' : form})
 
 def formulario_registro(request):
     return render(request, 'formulario_registro.html')
