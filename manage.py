a2df4153816e38ad68c122cb6472a8ac6504a9ad
J.Joaquin <quinoescobar@gmail.com>
2015-05-05 18:02:18 +0100

    no funciono lo que queria hacer

--- manage.py
+++ manage.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
 import os
 import sys
+#import unittest
+
+#from pretec.ext.script import Manager
+
+# @manager.command
+# def test():
+#     """Corre los tests sin coverage"""
+#     tests=unittest.TestLoader().discover('.')
+#     unittest.TextTestRunner(verbosity=2).run(tests)
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pretec.settings")
