32f29a2d3b5f76ab38ccc5082813fe648c71c6b2
alu0100763379 <alu0100763379@ull.edu.es>
2015-05-12 13:51:19 +0100

    microposts error en los models.py

--- microposts/models.py
+++ microposts/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django import forms
+from usuarios.models import Usuario
 # Create your models here.
 
 class Post(models.Model):
-	pseudonimo = models.CharField(primary_key = True, max_length = 30)
+	pseudonimo = models.ForeignKey(Usuario)
 	titulo = models.CharField(max_length=30)
-	texto = models.CharField(max_length=200)
+	texto = models.CharField(max_length=240)
 
 
