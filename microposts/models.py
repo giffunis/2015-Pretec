ba5bc8f229001627314af2a22f54ca9829bf5559
Rebecca Martinez Galan <rebe.mart.gal@gmail.com>
2015-05-12 12:59:31 +0100

    Creados los templates

--- microposts/models.py
+++ microposts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django import forms
 # Create your models here.
 
-class Post(models.Model):
+class Post(models.Model	
 	pseudonimo = models.CharField(primary_key = True, max_length = 30)
 	titulo = models.CharField(max_length=30)
 	texto = models.CharField(max_length=200)
