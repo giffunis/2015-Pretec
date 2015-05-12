c13c45d3d13f0ee636ac0d8ab027ae9e1a047f06
J.Joaquin <quinoescobar@gmail.com>
2015-05-12 13:34:11 +0100

    scriptfecha.js creado, falta ponerlo en el html

--- microposts/forms.py
+++ microposts/forms.py
@@ -4,7 +4,4 @@ from django.db import models
 class PostForm(forms.Form):
 	titulo = forms.CharField(label='Titulo', max_length=30, min_length=2)
 	texto = forms.CharField(label='Texto', max_length=200, min_length=5, widget=forms.Textarea)
-	
-
-
-
+	fecha = forms.DateField()
---
+++ static/js/scriptfecha.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    var d = new Date();
+    var n = d.toLocaleString();
+    document.getElementById("demo").innerHTML = n;
+}
