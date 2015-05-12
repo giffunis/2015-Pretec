0e736a0b5c35067d68614f5b1c5fb3a76bc5e724
Rebecca Martinez Galan <rebe.mart.gal@gmail.com>
2015-05-12 13:22:46 +0100

    creados los toemplates y modificado el models.py

--- microposts/forms.py
+++ microposts/forms.py
@@ -4,5 +4,7 @@ from django.db import models
 class PostForm(forms.Form):
 	titulo = forms.CharField(label='Titulo', max_length=30, min_length=2)
 	texto = forms.CharField(label='Texto', max_length=200, min_length=5, widget=forms.Textarea)
+	
+
 
 
--- microposts/models.py
+++ microposts/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 from django import forms
+import datetime
 # Create your models here.
 
-class Post(models.Model	
-	pseudonimo = models.CharField(primary_key = True, max_length = 30)
+class Post(models.Model):
 	titulo = models.CharField(max_length=30)
 	texto = models.CharField(max_length=200)
+
+	#devuelve el numero de post de un usuario
+	def mumero_microposts(pseudon):
+		total = Post.objects.get(pseudonimo = pseudon)
+		return len(total)
 
 
