8f35436d7cfd82f13ba4e8ab88a1912663084af7
Jose Saul Giffuni Becerra <giffunis@gmail.com>
2015-05-13 11:10:51 +0100

    solucionado error de    Cannot assign upiquesito: Post.pseudonimo must be a Usuario instance.

--- microposts/views.py
+++ microposts/views.py
@@ -5,7 +5,8 @@ from .forms import PostForm
 
 from microposts.models import Post
 from django.core.context_processors import csrf
-
+import usuarios
+from usuarios.models import Usuario
 
 # Create your views here.
 
@@ -19,7 +20,7 @@ def comprueba_auth(funcion):
     return comprueba_login
 
 
-
+@comprueba_auth
 def get_post(request):
     if request.method == 'POST':
         form=PostForm(request.POST)
@@ -28,9 +29,10 @@ def get_post(request):
             titulo = form.cleaned_data['titulo']
             texto = form.cleaned_data['texto']
             date  = form.cleaned_data['fecha']
+            usuario = Usuario.objects.get(pseudonimo = request.session['member_id'])
             #entrada en la base de datos
             post = Post.objects.create(
-	                        pseudonimo = request.session['member_id'],
+	                        pseudonimo = usuario,
 	                        titulo = titulo,
 	                        texto = texto,
 	                        fecha = date,)
@@ -38,4 +40,4 @@ def get_post(request):
             return render(request, 'micropost_enviado.html')
     else:
         form = PostForm()
-    return render(request, 'formulario_microposts.html', {'form' : form})
+    return render(request, 'formulario_microposts.html', {'pseudonimo':request.session['member_id'],'form' : form})
