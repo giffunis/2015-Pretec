0c8b99c52ded8e3e9109573909b7676ff4aa39f6
J.Joaquin <quinoescobar@gmail.com>
2015-05-19 15:36:17 +0100

    modificado para que se ponga fecha por defecto

--- microposts/forms.py
+++ microposts/forms.py
@@ -6,4 +6,4 @@ from bootstrap3_datetime.widgets import DateTimePicker
 class PostForm(forms.Form):
 	titulo = forms.CharField(label='Titulo', max_length=30, min_length=2)
 	texto = forms.CharField(label='Texto', max_length=200, min_length=5, widget=forms.Textarea)
-	fecha = forms.DateField(widget=DateTimePicker(options={"defaultDate":datetime.datetime.now(),"format": "MM-DD-YYYY","pickTime": False}))
+	fecha = forms.DateField(widget=DateTimePicker(options={"defaultDate":datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),"format": "DD/MM/YYYY","pickTime": False}))
