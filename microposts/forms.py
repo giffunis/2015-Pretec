343b078b6a51e5d79dbf4aa4b729063cb6e2622a
J.Joaquin <quinoescobar@gmail.com>
2015-05-19 14:27:04 +0100

    funcionando lo de la fecha

--- microposts/forms.py
+++ microposts/forms.py
@@ -1,7 +1,9 @@
 from django import forms
 from django.db import models
+import datetime
+from bootstrap3_datetime.widgets import DateTimePicker
 
 class PostForm(forms.Form):
 	titulo = forms.CharField(label='Titulo', max_length=30, min_length=2)
 	texto = forms.CharField(label='Texto', max_length=200, min_length=5, widget=forms.Textarea)
-	fecha = forms.DateField()
+	fecha = forms.DateField(widget=DateTimePicker(options={"defaultDate":datetime.datetime.now(),"format": "MM-DD-YYYY","pickTime": False}))
