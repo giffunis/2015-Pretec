38bdbe9a45b86257a58b8f9619c745fe9d1074de
J.Joaquin <quinoescobar@gmail.com>
2015-05-20 12:37:24 +0100

    micropost terminado

--- microposts/forms.py
+++ microposts/forms.py
@@ -6,4 +6,6 @@ from bootstrap3_datetime.widgets import DateTimePicker
 class PostForm(forms.Form):
 	titulo = forms.CharField(label='Titulo', max_length=30, min_length=2)
 	texto = forms.CharField(label='Texto', max_length=200, min_length=5, widget=forms.Textarea)
-	fecha = forms.DateField(widget=DateTimePicker(options={"defaultDate":datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),"format": "DD/MM/YYYY","pickTime": False}))
+	#fecha = forms.DateField(widget=DateTimePicker(options={"defaultDate":datetime.datetime.now().strftime("%Y-%m-%d"),"format": "DD/MM/YYYY","pickTime": False}))
+	#fecha2 = forms.DateField(widget=forms.HiddenInput())
+	fecha = forms.DateField(initial=datetime.datetime.now().strftime("%m/%d/%Y"))
