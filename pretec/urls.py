81e6c39eda58ab57563f2e0aa2040ec09f638f3e
Jose Saul Giffuni Becerra <giffunis@gmail.com>
2015-07-01 20:32:16 +0100

    arreglado lo del error de follow al mostrar seguidores

--- pretec/urls.py
+++ pretec/urls.py
@@ -23,7 +23,6 @@ urlpatterns = [
     url(r'^set_email/$', usuarios.views.set_email),
     url(r'^set_password/$', usuarios.views.set_password),
     url(r'^microposts/$', microposts.views.set_post),
-    url(r'^seguidores/$', usuarios.views.follow),
     url(r'^users/$', usuarios.views.users_view),
     url(r'^busquedaPosts/$', usuarios.views.buscarPosts),
     url(r'^busquedaUsu/$', usuarios.views.buscarUsuario),
