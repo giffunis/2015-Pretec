7182efe7d2eb87138fbc264d2cc49766a6562ed8
J.Joaquin <quinoescobar@gmail.com>
2015-07-04 21:07:05 +0100

    modificado los scripts de coverage and pylint and pymetrics

--- coverage_test.sh
+++ coverage_test.sh
@@ -5,8 +5,8 @@ rm -rf htmlcov/
 echo "Erasing previously collected data"
 coverage erase
 echo "Running coverage"
-coverage run --omit='*/__init__.py','*/views.py','*/forms.py','*/migrations/*.py','*/tests.py','ls */features/steps.py | tr "\n" ","' \
-    --source=usuarios \
+coverage run --omit='*/__init__.py','*/admin.py','*/views.py','*/forms.py','*/migrations/*.py','*/tests.py','ls */features/steps.py | tr "\n" ","' \
+    --source=usuarios,microposts \
     manage.py harvest
 echo "Test completed!"
 echo "Generating HTML report..."
--- pymeli.sh
+++ pymeli.sh
@@ -11,27 +11,11 @@ echo "Generating metrics for Usuarios  module..."
 pymetrics -i simple:SimpleMetric,mccabe:McCabeMetric usuarios/views.py usuarios/models.py > pymetrics/usuarios.txt
 pylint -d C0103,E1101 -f html usuarios/ > pylint/Usuarios.html
 
-# Metrics for frontend module
-#echo "Generating metrics for frontend module..."
-#pymetrics -i simple:SimpleMetric,mccabe:McCabeMetric frontend/views.py frontend/models.py > pymetrics/frontend.txt
-#pylint -d C0103,E1101 -f html frontend/ > pylint/frontend.html
-
-# Metrics for Photo module
-#echo "Generating metrics for Photo module..."
-#pymetrics -i simple:SimpleMetric,mccabe:McCabeMetric Photo/views.py Photo/models.py > pymetrics/Photo.txt
-#pylint -d C0103,E1101 -f html Photo/ > pylint/Photo.html
-
-# Metrics for Message module
-#echo "Generating metrics for Message module..."
-#pymetrics -i simple:SimpleMetric,mccabe:McCabeMetric Message/views.py Message/models.py > pymetrics/Message.txt
-#pylint -d C0103,E1101 -f html Message/ > pylint/Message.html
-
 # Metrics for Micropost module
-#echo "Generating metrics for Micropost module..."
-#pymetrics -i simple:SimpleMetric,mccabe:McCabeMetric Micropost/views.py Micropost/models.py > pymetrics/Micropost.txt
-#pylint -d C0103,E1101 -f html Micropost/ > pylint/Micropost.html
+echo "Generating metrics for frontend module..."
+pymetrics -i simple:SimpleMetric,mccabe:McCabeMetric microposts/views.py microposts/models.py > pymetrics/microposts.txt
+pylint -d C0103,E1101 -f html microposts/ > pylint/microposts.html
 
 # Cleaning temp files...
 echo "Cleaning temp files..."
 rm -f metricData.*
-
